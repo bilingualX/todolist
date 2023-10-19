7d014da8c51ecfb96e92a9b5887b465ab4e14e20
Gustavo D. Assunção <145854208+gustavo19972023@users.noreply.github.com>
2023-10-19 11:00:40 -0300

    doneaa

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/https://gustavo19972023.github.io/todolist/",
+  base: "/todolist/",
 })
